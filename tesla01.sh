810c8daf901dbd81baa81d03a344592e08a68d53
DJ ELUM <djelum85@gmail.com>
2023-09-07 00:25:37 +0000

    Joseph's commit

---
+++ tesla01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Prompt the user for their first name
+echo -n "Enter your first name: "
+read first_name
+
+# Prompt the user for their last name
+echo -n "Enter your last name: "
+read last_name
+
+# Print the first name and last name
+echo "Hello, $first_name $last_name!"
